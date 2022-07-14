1165bb8498d4c8879a68fe32f258f494c363da81
Chi-Chang <chichang@mindlogic.com.tw>
2022-07-14 18:29:05 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const path = require('path')  
 module.exports = {
-    baseUrl: process.env.NODE_ENV === "production" ? "./" : "",
+    publicPath: process.env.NODE_ENV === "production" ? "/" : "",
     assetsDir:"",
     outputDir: "dist",
 }
