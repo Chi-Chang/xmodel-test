67f222cf3036bf17d38d25481d514658f524e75e
Chi-Chang <chichang@mindlogic.com.tw>
2022-07-14 15:52:14 +0800

    test

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  configureWebpack: {
-    output: {
-        publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
-    }
-}
+  ]
 }
